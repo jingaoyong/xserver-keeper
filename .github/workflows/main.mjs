0d8eca48d35e1db1a372504148496a9c04eadfc0
snufkin <56209391+jingaoyong@users.noreply.github.com>
2025-11-11 17:26:07 +0800

    Add main.mjs workflow for Puppeteer and Telegram

---
+++ .github/workflows/main.mjs
@@ -0,0 +1,10 @@
+# 安装依赖
+yarn add puppeteer node-fetch
+
+# 设置环境变量
+export ACCOUNTS='[{"jingaoyong@gmail.com": "iA4b6uqH45JL.XP"}]'
+export TELEGRAM_BOT_TOKEN="8238088809:AAFvajqwozG30V9Xg8e8bZGUAWfiz62PcHc"
+export TELEGRAM_CHAT_ID="6986547845"
+
+# 运行程序
+node main.mjs
